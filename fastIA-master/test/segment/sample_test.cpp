003bcacc6be223da1cd7136715f7291b338e6f66
admin.michalc <admin.michalc@gmail.com>
2013-06-15 14:25:44 +0000

    test data imported (converted to .png), ctest configuration improved

--- fastIA-master/test/segment/sample_test.cpp
+++ fastIA-master/test/segment/sample_test.cpp
@@ -2,11 +2,15 @@
 #define BOOST_TEST_DYN_LINK
 
 #include <boost/test/unit_test.hpp>
+#include "TestUtils.h"
 
 #include <stdio.h>
 //#include "PixelOperations.h"
 #include "UtilsCVImageIO.h"
 #include "ConnComponents.h"
+
+#include "opencv2/opencv.hpp"
+
 #if defined (WITH_CUDA)
 #include "opencv2/gpu/stream_accessor.hpp"
 
@@ -14,22 +18,31 @@
 #include "ccl_uf.cuh"
 #endif
 
+using namespace cv;
 
-BOOST_AUTO_TEST_CASE( test1 )
+BOOST_AUTO_TEST_CASE(test1)
 {
     BOOST_CHECK(2 == 2);
+    
+    std::cout << RESOURCE("blabla.jpg") << std::endl;
+    
+    Mat img = imread(RESOURCE("text.png"));
+    
+    std::cout << img.size();
+    
+    
+//    uint64_t t1, t2;
+//    std::vector<int> stages;
+//    for (int stage = 0; stage <= 200; ++stage) {
+//        stages.push_back(stage);
+//    }
+    
 
-    uint64_t t1, t2;
-    std::vector<int> stages;
-    for (int stage = 0; stage <= 200; ++stage) {
-        stages.push_back(stage);
-    }
-
-    std::string prefix;
-    prefix.assign("test/out-bwareaopen-test-");
-    std::string suffix;
-    suffix.assign(".pbm");
-    ::cciutils::cv::IntermediateResultHandler *iwrite = new ::cciutils::cv::IntermediateResultWriter(prefix, suffix, stages);
+//    std::string prefix;
+//    prefix.assign("test/out-bwareaopen-test-");
+//    std::string suffix;
+//    suffix.assign(".pbm");
+//    ::cciutils::cv::IntermediateResultHandler *iwrite = new ::cciutils::cv::IntermediateResultWriter(prefix, suffix, stages);
 
 }
 
