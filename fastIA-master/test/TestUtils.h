32b0a84bc2caf735ffa767a3cea4e36785b46740
admin.michalc <admin.michalc@gmail.com>
2013-06-15 16:01:38 +0000

    some simple test, cleaning

--- fastIA-master/test/TestUtils.h
+++ fastIA-master/test/TestUtils.h
@@ -1,6 +1,7 @@
 #ifndef TEST_UTILS_H_
 #define TEST_UTILS_H_
 
-#define RESOURCE(path) TEST_DATA_PATH "/" path
+#define DATA_IN(path) TEST_DATA_PATH "/" path
+#define DATA_OUT(path) "out/" path
 
 #endif
--- fastIA-master/test/segment/sample_test.cpp
+++ fastIA-master/test/segment/sample_test.cpp
@@ -5,44 +5,18 @@
 #include "TestUtils.h"
 
 #include <stdio.h>
-//#include "PixelOperations.h"
-#include "UtilsCVImageIO.h"
-#include "ConnComponents.h"
-
 #include "opencv2/opencv.hpp"
-
-#if defined (WITH_CUDA)
-#include "opencv2/gpu/stream_accessor.hpp"
-
-#include "opencv2/gpu/gpu.hpp"
-#include "ccl_uf.cuh"
-#endif
 
 using namespace cv;
 
 BOOST_AUTO_TEST_CASE(test1)
-{
-    BOOST_CHECK(2 == 2);
+{   
+    std::cout << DATA_IN("blabla.jpg") << std::endl;
     
-    std::cout << RESOURCE("blabla.jpg") << std::endl;
+    Mat img = imread(DATA_IN("text.png"));
     
-    Mat img = imread(RESOURCE("text.png"));
+    BOOST_CHECK(img.data);
     
-    std::cout << img.size();
-    
-    
-//    uint64_t t1, t2;
-//    std::vector<int> stages;
-//    for (int stage = 0; stage <= 200; ++stage) {
-//        stages.push_back(stage);
-//    }
-    
-
-//    std::string prefix;
-//    prefix.assign("test/out-bwareaopen-test-");
-//    std::string suffix;
-//    suffix.assign(".pbm");
-//    ::cciutils::cv::IntermediateResultHandler *iwrite = new ::cciutils::cv::IntermediateResultWriter(prefix, suffix, stages);
-
+    imwrite(DATA_OUT("text.png"), img);
 }
 
