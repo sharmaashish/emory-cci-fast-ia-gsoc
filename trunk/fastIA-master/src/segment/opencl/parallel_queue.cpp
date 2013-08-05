e954781e153638b6e03fdf0617b0665b171b32e6
admin.michalc@gmail.com <admin.michalc@gmail.com@c32e06d3-177a-4434-3b00-e3de7b896539>
2013-08-05 16:38:46 +0000

    porting parallel queues to OpenCL

--- trunk/fastIA-master/src/segment/opencl/parallel_queue.cpp
+++ trunk/fastIA-master/src/segment/opencl/parallel_queue.cpp
@@ -58,3 +58,50 @@ void dequeueTest(cl::Buffer& device_result,
     cl_int status = queue.enqueueNDRangeKernel(dequeue_test_kernel,
                                                nullRange, global, local);
 }
+
+#define QUEUE_WARP_SIZE 	32
+#define QUEUE_NUM_THREADS	512
+#define QUEUE_NUM_WARPS (QUEUE_NUM_THREADS / QUEUE_WARP_SIZE)
+#define LOG_QUEUE_NUM_THREADS 9
+#define LOG_QUEUE_NUM_WARPS (LOG_QUEUE_NUM_THREADS - 5)
+#define QUEUE_SCAN_STRIDE (QUEUE_WARP_SIZE + QUEUE_WARP_SIZE / 2 + 1)
+
+void sumTest(cl::Buffer& device_result, int iterations,
+                 ProgramCache& cache,
+                 cl::CommandQueue& queue)
+{
+    cl::Context context = queue.getInfo<CL_QUEUE_CONTEXT>();
+
+    std::stringstream params_stream;
+    std::string program_params = params_stream.str();
+
+    cl::Program& program = cache.getProgram("ParallelQueue", program_params);
+
+    cl::Kernel sum_test_kernel(program, "sum_test");
+
+    cl::LocalSpaceArg local_queue = cl::__local(sizeof(int) * QUEUE_NUM_THREADS * 2);
+    cl::LocalSpaceArg reduction_buffer = cl::__local(sizeof(int) * QUEUE_NUM_THREADS);
+    cl::LocalSpaceArg got_work = cl::__local(sizeof(int));
+    cl::LocalSpaceArg prefix_sum_input = cl::__local(sizeof(int) * QUEUE_NUM_THREADS);
+    cl::LocalSpaceArg prefix_sum_output = cl::__local(sizeof(int) * QUEUE_NUM_THREADS);
+    cl::LocalSpaceArg prefix_sum_workspace_1 = cl::__local(sizeof(int) * QUEUE_NUM_WARPS * QUEUE_SCAN_STRIDE);
+    cl::LocalSpaceArg prefix_sum_workspace_2 = cl::__local(sizeof(int) * QUEUE_NUM_WARPS + QUEUE_NUM_WARPS / 2);
+
+    sum_test_kernel.setArg(0, device_result);
+    sum_test_kernel.setArg(1, iterations);
+    sum_test_kernel.setArg(2, local_queue);
+    sum_test_kernel.setArg(3, reduction_buffer);
+    sum_test_kernel.setArg(4, got_work);
+    sum_test_kernel.setArg(5, prefix_sum_input);
+    sum_test_kernel.setArg(6, prefix_sum_output);
+    sum_test_kernel.setArg(7, prefix_sum_workspace_1);
+    sum_test_kernel.setArg(8, prefix_sum_workspace_2);
+
+    cl::NDRange nullRange;
+    cl::NDRange global(512, 1);
+    cl::NDRange local(512, 1);
+
+    cl_int status = queue.enqueueNDRangeKernel(sum_test_kernel,
+                                               nullRange, global, local);
+}
+
--- trunk/fastIA-master/test/segment/parallelQueueTest.cpp
+++ trunk/fastIA-master/test/segment/parallelQueueTest.cpp
@@ -72,3 +72,65 @@ BOOST_AUTO_TEST_CASE(queue_init_test)
     std::cout << "init queue test finished" << std::endl;
 }
 
+
+BOOST_AUTO_TEST_CASE(queue_sum_test)
+{
+    std::cout << "parallel queue sum test hello" << std::endl;
+
+    cl_int err = CL_SUCCESS;
+
+    const int input_size = 1024;
+    const int output_size = 1024;
+    //const int result_size = 512;
+
+    const int iterations = 2;
+    const int result_size = 1;
+
+
+    const int input_size_bytes = input_size * sizeof(int);
+    const int output_size_bytes = output_size * sizeof(int);
+    const int result_size_bytes = result_size * sizeof(int);
+
+    try
+    {
+        cl::CommandQueue queue = ProgramCache::getGlobalInstance().getDefaultCommandQueue();
+        cl::Context context = queue.getInfo<CL_QUEUE_CONTEXT>();
+
+        int host_in[input_size];
+        int host_out[output_size];
+        int host_result[result_size];
+
+        std::fill(host_in, host_in + input_size, 1);
+        std::fill(host_out, host_out + output_size, 0);
+
+        cl::Buffer device_in(context, CL_TRUE, input_size_bytes);
+        queue.enqueueWriteBuffer(device_in, CL_TRUE, 0, input_size_bytes, host_in);
+
+        cl::Buffer device_out(context, CL_TRUE, output_size_bytes);
+        queue.enqueueWriteBuffer(device_out, CL_TRUE, 0, output_size_bytes, host_out);
+
+        // initialize queue
+        initQueue(device_in, input_size, device_out, output_size);
+
+        cl::Buffer device_result(context, CL_TRUE, result_size_bytes);
+
+        std::cout << "parallel sum..." << std::endl;
+        //dequeueTest(device_result);
+        sumTest(device_result, iterations);
+
+        queue.enqueueReadBuffer(device_result, CL_TRUE, 0, result_size_bytes, host_result);
+
+        for(int i = 0; i < result_size; ++i)
+        {
+            std::cout << "result[" << i << "] = " << host_result[i] << std::endl;
+        }
+
+    }
+    catch (cl::Error err)
+    {
+        oclPrintError(err);
+    }
+
+    std::cout << "sum queue test finished" << std::endl;
+}
+
