bce435814b47364fda9c32569cbc4575180862ff
admin.michalc <admin.michalc@gmail.com>
2013-08-07 15:15:28 +0000

    parallel queues - pattern for global memory allocation changed

--- fastIA-master/src/segment/opencl/parallel_queue.cpp
+++ fastIA-master/src/segment/opencl/parallel_queue.cpp
@@ -3,6 +3,27 @@
 
 #include <iostream>
 
+#define QUEUE_MAX_NUM_BLOCKS	70
+#define QUEUE_NUM_THREADS	    512
+
+static cl::Buffer queue_workspace;
+
+void initQueueSystem(int queueMaxNumBlocks, cl::CommandQueue& queue)
+{
+    cl::Device device = queue.getInfo<CL_QUEUE_DEVICE>();
+    int addressSize = device.getInfo<CL_DEVICE_ADDRESS_BITS>()/8;
+
+    int byteSize = 5 * sizeof(int) * QUEUE_MAX_NUM_BLOCKS
+            + 4 * addressSize * QUEUE_MAX_NUM_BLOCKS + sizeof(int);
+
+    cl::Context context = queue.getInfo<CL_QUEUE_CONTEXT>();
+    queue_workspace = cl::Buffer(context, CL_TRUE, byteSize);
+}
+
+void disposeQueueSystem()
+{
+    //queue_workspace.release();
+}
 
 void initQueue(cl::Buffer& inQueueData, int dataElements,
                cl::Buffer& outQueueData, int outMaxSize,
@@ -18,10 +39,11 @@ void initQueue(cl::Buffer& inQueueData, int dataElements,
 
     cl::Kernel init_queue_kernel(program, "init_queue_kernel");
 
-    init_queue_kernel.setArg(0, inQueueData);
-    init_queue_kernel.setArg(1, dataElements);
-    init_queue_kernel.setArg(2, outQueueData);
-    init_queue_kernel.setArg(3, outMaxSize);
+    init_queue_kernel.setArg(0, queue_workspace);
+    init_queue_kernel.setArg(1, inQueueData);
+    init_queue_kernel.setArg(2, dataElements);
+    init_queue_kernel.setArg(3, outQueueData);
+    init_queue_kernel.setArg(4, outMaxSize);
 
     cl::NDRange nullRange;
     cl::NDRange global(1, 1);
@@ -48,8 +70,9 @@ void dequeueTest(cl::Buffer& device_result,
 
     cl::LocalSpaceArg local_mem = cl::__local(sizeof(int));
 
-    dequeue_test_kernel.setArg(0, device_result);
-    dequeue_test_kernel.setArg(1, local_mem);
+    dequeue_test_kernel.setArg(0, queue_workspace);
+    dequeue_test_kernel.setArg(1, device_result);
+    dequeue_test_kernel.setArg(2, local_mem);
 
     cl::NDRange nullRange;
     cl::NDRange global(512, 1);
@@ -86,13 +109,14 @@ void sumTest(cl::Buffer& device_result, int iterations,
     cl::LocalSpaceArg prefix_sum_input = cl::__local(sizeof(int) * QUEUE_NUM_THREADS);
     cl::LocalSpaceArg prefix_sum_output = cl::__local(sizeof(int) * QUEUE_NUM_THREADS);
 
-    sum_test_kernel.setArg(0, device_result);
-    sum_test_kernel.setArg(1, iterations);
-    sum_test_kernel.setArg(2, local_queue);
-    sum_test_kernel.setArg(3, reduction_buffer);
-    sum_test_kernel.setArg(4, got_work);
-    sum_test_kernel.setArg(5, prefix_sum_input);
-    sum_test_kernel.setArg(6, prefix_sum_output);
+    sum_test_kernel.setArg(0, queue_workspace);
+    sum_test_kernel.setArg(1, device_result);
+    sum_test_kernel.setArg(2, iterations);
+    sum_test_kernel.setArg(3, local_queue);
+    sum_test_kernel.setArg(4, reduction_buffer);
+    sum_test_kernel.setArg(5, got_work);
+    sum_test_kernel.setArg(6, prefix_sum_input);
+    sum_test_kernel.setArg(7, prefix_sum_output);
 
     cl::NDRange nullRange;
     cl::NDRange global(QUEUE_NUM_THREADS, 1);
--- fastIA-master/src/segment/opencl/parallel_queue.h
+++ fastIA-master/src/segment/opencl/parallel_queue.h
@@ -4,6 +4,11 @@
 #include <CL/cl.hpp>
 #include "utils/ocl_program_cache.h"
 
+void initQueueSystem(int queueMaxNumBlocks,
+                     cl::CommandQueue& queue = ProgramCache::getGlobalInstance().getDefaultCommandQueue());
+
+void disposeQueueSystem();
+
 void initQueue(cl::Buffer& inQueueData, int dataElements,
                cl::Buffer& outQueueData, int outMaxSize,
                ProgramCache& cache = ProgramCache::getGlobalInstance(),
