bde79ddb994f36ed66e4c5b3fd5f3c11c948a362
admin.michalc <admin.michalc@gmail.com>
2013-08-23 19:24:05 +0000

    queue based morph recon, development and testing

--- fastIA-master/src/segment/opencl/morph_recon.h
+++ fastIA-master/src/segment/opencl/morph_recon.h
@@ -4,6 +4,12 @@
 #include <CL/cl.hpp>
 #include "utils/ocl_program_cache.h"
 
+void morphReconInit(cl::Buffer seends, cl::Buffer image,
+                    int width, int height,
+                    ProgramCache& cache = ProgramCache::getGlobalInstance(),
+                    cl::CommandQueue& queue = ProgramCache::getGlobalInstance()
+                                                   .getDefaultCommandQueue());
+
 void morphRecon(cl::Buffer input_list, int dataElements,
                 cl::Buffer seeds,cl::Buffer image, int ncols, int nrows,
                 ProgramCache& cache = ProgramCache::getGlobalInstance(),
